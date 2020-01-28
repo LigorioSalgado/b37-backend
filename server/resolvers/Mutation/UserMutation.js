43a85195d89c1b80829f56d02272c9ea3ae8d972
ligorio93 <edwin@devf.mx>
2020-01-27 20:25:45 -0600

    finish crud

--- server/resolvers/Mutation/UserMutation.js
+++ server/resolvers/Mutation/UserMutation.js
@@ -1,4 +1,4 @@
-const { createUser } =  require('../../services/UserService');
+const { createUser, updateUserByID, getUserByID, deleteUserByID } =  require('../../services/UserService');
 
 const newUser = (root,args,context,info) => {
     
@@ -6,8 +6,25 @@ const newUser = (root,args,context,info) => {
 
 };
 
+const updateUser = async(_,args) => {
+	await updateUserByID(args.id, args.data);
+	return getUserByID(args.id);
+};
+
+const deleteUser = async(_, args) => {
+	await deleteUserByID(args.id);
+	return {
+		code:204,
+		message:'User removed successfully'
+	};
+
+};
+
 
 
 module.exports = {
-	newUser
+	newUser,
+	updateUser,
+	deleteUser,
+
 };
